8f21bd15c50ac0dea4b1dcebb5dfa84a3539464d
parissakalaee <parissa.kalaee@officebooking.com>
2023-04-13 09:58:26 +0200

    fix: Migrate to AndroidX

--- app/src/main/java/com/example/nfcapp/MainActivity.java
+++ app/src/main/java/com/example/nfcapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.nfcapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import androidx.navigation.NavController;
